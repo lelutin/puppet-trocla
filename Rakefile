68277a61cc0cd14f2ba896b1b2a4517fef3c307b
Gabriel Filion <gabster@lelutin.ca>
2025-05-29 18:03:18 -0400

    [WIP] simplify rakefile and spec_helper file

--- Rakefile
+++ Rakefile
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
-require 'bundler'
-Bundler.require(:rake)
-
 require 'puppetlabs_spec_helper/rake_tasks'
 require 'puppet-syntax/tasks/puppet-syntax'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,4 @@
 # frozen_string_literal: true
 
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rake'
-
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-
-RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
-  c.pattern = FileList[c.pattern].exclude(%r{^spec/fixtures})
-  c.confdir = '/etc/puppet'
-end
-
-Puppet::Util::Log.level = :warning
-Puppet::Util::Log.newdestination(:console)
+require 'rspec-puppet-facts'
